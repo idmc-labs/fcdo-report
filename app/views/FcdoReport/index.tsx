cf8fcb8e8cb12dacdb5ef2a6d2d524542d63ed6f
barshathakuri <barshathakuri708@gmail.com>
2022-10-31 11:34:32 +0545

    fix new feedbacks

--- app/views/FcdoReport/index.tsx
+++ app/views/FcdoReport/index.tsx
@@ -266,14 +266,6 @@ function FcdoReport(props: Props) {
                     )}
                 >
                     <Button
-                        name="at-a-glance"
-                        onClick={handleNavClick}
-                        className={styles.navItem}
-                        variant="transparent"
-                    >
-                        At a glance
-                    </Button>
-                    <Button
                         name="key-findings"
                         onClick={handleNavClick}
                         className={styles.navItem}
@@ -287,7 +279,7 @@ function FcdoReport(props: Props) {
                         className={styles.navItem}
                         variant="transparent"
                     >
-                        Explore the data
+                        In summary
                     </Button>
                     <Button
                         name="download-report"
@@ -295,7 +287,7 @@ function FcdoReport(props: Props) {
                         className={styles.navItem}
                         variant="transparent"
                     >
-                        Download report
+                        The Way Forward & Conclusion
                     </Button>
                 </div>
             </nav>
@@ -559,11 +551,13 @@ function FcdoReport(props: Props) {
                             <p className={styles.descriptionCaption}>
                                 {descriptionCaption}
                             </p>
-                            <Quote
-                                quote={qualityEducationQuote}
-                                author={qualityEducationAuthor}
-                            />
                         </div>
+                    </div>
+                    <div className={styles.quotes}>
+                        <Quote
+                            quote={qualityEducationQuote}
+                            author={qualityEducationAuthor}
+                        />
                     </div>
                 </div>
             </section>
@@ -580,6 +574,12 @@ function FcdoReport(props: Props) {
                                     src={spotLight1}
                                     alt=""
                                 />
+                                <div className={styles.caption}>
+                                    <div className={styles.subHeading}>
+                                        Gender disparities in access to
+                                        education in Somalia and Ethiopia
+                                    </div>
+                                </div>
                             </div>
                             <div className={styles.spotlightItem}>
                                 <img
@@ -587,6 +587,12 @@ function FcdoReport(props: Props) {
                                     src={spotLight2}
                                     alt=""
                                 />
+                                <div className={styles.caption}>
+                                    <div className={styles.subHeading}>
+                                        Estimating the number of IDPs at
+                                        risk of missing out on education
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -633,7 +639,7 @@ function FcdoReport(props: Props) {
                         heading="Conclusion"
                         headingSize="large"
                     />
-                    <div className={styles.idConclusionTopContainer}>
+                    <div className={styles.description}>
                         <p className={styles.descriptionParagraph}>
                             {conclusionDescription}
                         </p>
